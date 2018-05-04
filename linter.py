2ad5ad7adba085bcb6877971d50276fcbf6fc62e
Ankit Gahlawat <ankit5000@ymail.com>
2018-05-05 04:01:08 +0530

    update linter.py
    added indentation module

--- linter.py
+++ linter.py
@@ -1,31 +1,25 @@
 import sys
+import re
 
-# with open("3.pl") as f:
-#     content = f.readlines()
-# # you may also want to remove whitespace characters like `\n` at the end of each line
-# content = [x.strip() for x in content] 
-
-# print len(content[0])
-
-# # for i in range(len(content)):
-# # 	print len(content[i])
-# print content[2][10]
+OPENING_BRACKET_REGEX = re.compile("[\{]")
+CLOSING_BRACKET_REGEX = re.compile("[\}]")
 
 class Linter:
 
 	def __init__(self,filePath):
 		self.content = []
 		self.readFile(filePath)
-		self.maxLineLength = 60
+		self.maxLineLength = 600
 
 	def getScore(self):
-		self.testLineLength()	
+		self.testLineLength()
+		print self.indentation()
 
 	def readFile(self,filePath):
 		with open(filePath) as f:
 			self.content = f.readlines()
 		# you may also want to remove whitespace characters like `\n` at the end of each line
-		self.content = [x.strip() for x in self.content] 
+		#self.content = [x.strip() for x in self.content] 
 
 
 	def printContent(self):
@@ -36,13 +30,55 @@ class Linter:
  	def testLineLength(self):
  		for i in range(len(self.content)):
  			if(len(self.content[i]) > self.maxLineLength):
- 				print "this line is too long: " + self.content[i]
+ 				print "this line is too long: " + self.content[i]			
 
- 				
+ 	def indentation(self):
+ 		curlyNumber = 0
+ 		numberOfLines = len(self.content)
+ 		fail = 0
+ 		success = 0
+ 		for i in range(len(self.content)):
 
+ 			countSpace = True
+ 			numberOfSpaces = 0
+
+ 			# if(OPENING_BRACKET_REGEX.search(self.content[i])):
+ 			# 	curlyNumber += 1
+
+ 			# if(CLOSING_BRACKET_REGEX.search(self.content[i])):
+ 			# 	curlyNumber -= 1
+
+ 			# if(re.match("[\t]{" + str(curlyNumber) + "}\w*", self.content[i])):
+ 			# 	success += 1
+
+ 			for j in range(len(self.content[i])):
+
+ 				if(self.content[i][j] == ' ' and countSpace == True):
+ 					numberOfSpaces += 1
+
+ 				if(self.content[i][j] == '\t' and countSpace == True):
+ 					numberOfSpaces += 4
+
+ 				if(self.content[i][j] == '{'):
+ 					curlyNumber += 1
+ 					countSpace = False
+
+ 				elif(self.content[i][j] == '}'):
+ 					curlyNumber -= 1
+ 					countSpace = False
+
+ 				if((self.content[i][j] != ' ' and self.content[i][j] != '\t') and countSpace == True):
+ 					countSpace = False
+
+ 					if(numberOfSpaces != 4*curlyNumber):
+ 						fail += 1
+
+ 		return (float(numberOfLines - fail)/numberOfLines)*100.0			
+
+ 						
 
 if __name__ == '__main__':
-	myLint =  Linter(sys.argv[1])
+	myLint = Linter(sys.argv[1])
 	myLint.getScore()		
 
 
