b5f56d6c477b5c65c900e8de1a71203d676547ef
udjindal <jindaludit23@gmail.com>
2018-05-05 05:21:30 +0530

    Added comment functionality

--- linter.py
+++ linter.py
@@ -4,6 +4,9 @@ import re
 OPENING_BRACKET_REGEX = re.compile("[\{]")
 CLOSING_BRACKET_REGEX = re.compile("[\}]")
 
+COMMENT_1_REGEX = re.compile(r'\/{2}')
+COMMENT_2_OPEN_REGEX = re.compile(r'\/\*')
+COMMENT_2_CLOSE_REGEX = re.compile(r'\*\/')
 class Linter:
 
 	def __init__(self,filePath):
@@ -14,12 +17,12 @@ class Linter:
 	def getScore(self):
 		self.testLineLength()
 		print self.indentation()
-
+		self.comment()
 	def readFile(self,filePath):
 		with open(filePath) as f:
 			self.content = f.readlines()
 		# you may also want to remove whitespace characters like `\n` at the end of each line
-		#self.content = [x.strip() for x in self.content] 
+		#self.content = [x.strip() for x in self.content]
 
 
 	def printContent(self):
@@ -30,7 +33,7 @@ class Linter:
  	def testLineLength(self):
  		for i in range(len(self.content)):
  			if(len(self.content[i]) > self.maxLineLength):
- 				print "this line is too long: " + self.content[i]			
+ 				print "this line is too long: " + self.content[i]
 
  	def indentation(self):
  		curlyNumber = 0
@@ -55,10 +58,28 @@ class Linter:
  			not(re.match("[\ ]{" + str(curlyNumber*4) + "}[^\t\ ]", self.content[i])) and checkCurly == False):
  				fail += 1
 
- 		return (float(numberOfLines - fail)/numberOfLines)*100.0			
+ 		return (float(numberOfLines - fail)/numberOfLines)*100.0
 
- 						
+ 	def comment(self):
+		cnt = 0
+		check = False
+		for strng in self.content:
+			if(COMMENT_1_REGEX.search(strng)):
+				cnt += 1
+		temp = 0;
+		for strng in self.content:
+			if(COMMENT_2_OPEN_REGEX.search(strng)):
+				temp = 1
+				check = True
+			elif(COMMENT_2_CLOSE_REGEX.search(strng) and check):
+				temp += 1
+				check = False
+				cnt += temp
+				temp = 0
+			elif(check):
+				temp += 1
+		print cnt
 
 if __name__ == '__main__':
 	myLint = Linter(sys.argv[1])
-	myLint.getScore()		
+	myLint.getScore()
--- testfiles/virtual.cpp
+++ testfiles/virtual.cpp
@@ -1,38 +1,47 @@
-#include 
-using namespace std; 
+#include
+using namespace std;
+uditt // usd
+
+/*udf
+fddg
+sa
+as*/
+
+
+
 
 class b
 {
-public:
-virtual void show()
-{
-cout<<"\n  Showing base class....";
-}
-void display()
-{
-cout<<"\n  Displaying base class...." ;
-}
+    public:
+    virtual void show() // jisdcf
+    {
+        cout<<"\n  Showing base class....";
+    }
+    void display()
+    {
+        cout<<"\n  Displaying base class...." ;
+    }
 };
 
 class d:public b
 {
-public:
-void display()
-{
-cout<<"\n  Displaying derived class....";
-}
-void show()
-{
-cout<<"\n  Showing derived class....";
-}
+    public:
+    void display()
+    {
+        cout<<"\n  Displaying derived class....";
+    }
+    void show()
+    {
+        cout<<"\n  Showing derived class....";
+    }
 };
 
 int main()
 {
-b B;
-b *ptr;
-cout<<"\n\t P points to base:\n" ; ptr=&B; ptr->display();
-ptr->show();
-cout<<"\n\n\t P points to drive:\n"; d D; ptr=&D; ptr->display();
-ptr->show();
+    b B;
+    b *ptr;
+    cout<<"\n\t P points to base:\n" ; ptr=&B; ptr->display();
+    ptr->show();
+    cout<<"\n\n\t P points to drive:\n"; d D; ptr=&D; ptr->display();
+    ptr->show();
 }
